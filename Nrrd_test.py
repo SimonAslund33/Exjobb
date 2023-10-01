4d6f567335ba14643d8ceb82bb2fc3f491506d30
SimonAslund33 <114583924+SimonAslund33@users.noreply.github.com>
2023-10-01 19:35:43 +0200

    Add files via upload

--- Nrrd_test.py
+++ Nrrd_test.py
@@ -1,96 +1,101 @@
 import numpy as np
 import napari
 import nrrd
-import os
-import cv2
+#import os
+#import cv2
 import glob
 import json
-import pyvista as pv
+#import pyvista as pv
 from skspatial.objects import Line, Points
-from skspatial.plotting import plot_3d
+#from skspatial.plotting import plot_3d
 from matplotlib import pyplot as plt
-from scipy.optimize import curve_fit
-from scipy.interpolate import Rbf
-from scipy.interpolate import CubicSpline
-from mpl_toolkits import mplot3d
+#from scipy.optimize import curve_fit
+#from scipy.interpolate import Rbf
+#from scipy.interpolate import CubicSpline
+#from mpl_toolkits import mplot3d
 import math
-
-Patient = r"\BiKE_0843"
-Dir = r"C:\Users\simon\OneDrive\Desktop\ExJobbPlaqueInfo"
-suffix_comp = r"\*\RightCarotid\Batch*__composition.multi.nrrd"
-suffix_Lumen = r"\*\RightCarotid\*lumenSegmentation.nrrd"
-suffix_Wall = r"\*\RightCarotid\*wallSegmentation.nrrd"
-suffix_Donut = r"\*\RightCarotid\CommonCarotidArtery/donut"
-suffix_save = r"\*\RightCarotid/"
-suffix_Json = r"\*\RightCarotid\Batch*__lesionReadings.json"
-
+#"E:\BiKE_0830"
+def create_file_paths(Patient):
+    Patient = r"/" + Patient
+    Dir = r"E:/"
+    suffix_comp = r"\*\RightCarotid\Batch*__composition.multi.nrrd"
+    suffix_Lumen = r"\*\RightCarotid\*lumenSegmentation.nrrd"
+    suffix_Wall = r"\*\RightCarotid\*wallSegmentation.nrrd"
+    #suffix_Donut = r"\*\RightCarotid\CommonCarotidArtery/donut"
+    suffix_save = r"\*\RightCarotid/"
+    suffix_Json = r"\*\RightCarotid\Batch*__lesionReadings.json"
 
 #filepath1 = glob.glob(r'C:\Users\simon\OneDrive\Desktop\ExJobbPlaqueInfo\BiKE_0830\*\RightCarotid\Batch*__composition.multi.nrrd')
-filepath1 = glob.glob(Dir + Patient + suffix_comp)
+    filepath1 = glob.glob(Dir + Patient + suffix_comp)
 #print(filepath1[0])
 
-filepath2 = glob.glob(Dir + Patient + suffix_Lumen)
+    filepath2 = glob.glob(Dir + Patient + suffix_Lumen)
 #print(filepath2[0])
 
-filepath3 = glob.glob(Dir + Patient + suffix_Wall)
+    filepath3 = glob.glob(Dir + Patient + suffix_Wall)
 #print(filepath3[0])
-filepath4 = glob.glob(Dir + Patient + suffix_save)
-print(filepath4)
-filepath5 = glob.glob(Dir + Patient + suffix_Json)
-print(filepath5)
-readings_d = json.load(open(filepath5[0]))
-CompositionData1, header2 = nrrd.read(filepath1[0])
+    filepath4 = glob.glob(Dir + Patient + suffix_save)
+#print(filepath4)
+    filepath5 = glob.glob(Dir + Patient + suffix_Json)
+    return filepath1,filepath2,filepath3,filepath4,filepath5
 
-LumenData, header2 = nrrd.read(filepath2[0])
-WallData, header3 = nrrd.read(filepath3[0])
+def create_volume(f1,f2,f3,f4,f5):
 
 
-comp = np.zeros_like(CompositionData1[:,:,:,0])
+#print(filepath5)
+    readings_d = json.load(open(f5[0]))
+    CompositionData1, header2 = nrrd.read(f1[0])
 
-for i in range(5):
+    LumenData, header2 = nrrd.read(f2[0])
+    WallData, header3 = nrrd.read(f3[0])
+    print(CompositionData1.shape)
+
+    comp = np.zeros_like(CompositionData1[:,:,:,0])
+
+    for i in range(5):
     # 1 - CALC
     # 2 - LRNC
     # 3 - IPH
     # 4 - PVAT
     # 5 - MATX
-    hej = CompositionData1[:,:,:,i]
-    i = i+1
+        hej = CompositionData1[:,:,:,i]
+        i = i+1
 
-    if i == 1:
-        hej[hej > 0] = 0
-        hej[hej < 0] = i
-    elif i == 2:
-        hej[hej > 0] = 0
-        hej[hej < 0] = i
-    elif i == 3:
-        hej[hej > 0] = 0
-        hej[hej < 0] = i
-    elif i == 4:
-        hej[hej > 0] = 0
-        hej[hej < 0] = 0
-    elif i == 5:
-        hej[hej > 0] = 0
-        hej[hej < 0] = i
-    else:
-        print("what")
-    comp = comp + hej
+        if i == 1:
+            hej[hej > 0] = 0
+            hej[hej < 0] = i
+        elif i == 2:
+            hej[hej > 0] = 0
+            hej[hej < 0] = i
+        elif i == 3:
+            hej[hej > 0] = 0
+            hej[hej < 0] = i
+        elif i == 4:
+            hej[hej > 0] = 0
+            hej[hej < 0] = 0
+        elif i == 5:
+            hej[hej > 0] = 0
+            hej[hej < 0] = i
+        else:
+            print("what")
+        comp = comp + hej
   
 
-LumenData[LumenData > 0] = 0
-LumenData[LumenData < 0] = 6
+    LumenData[LumenData > 0] = 0
+    LumenData[LumenData < 0] = 6
 
 
 
 #6 - LUMEN
-labelVolume = comp + LumenData
+    labelVolume = comp + LumenData
 
 
 
-Original_shape = labelVolume.shape
-
+    Original_shape = labelVolume.shape
+    return labelVolume,readings_d,LumenData
     
 
-def build_Center_Line(x,y):
+def build_Center_Line(x,y,x2,y2, readings_d, labelVolume):
 
     root = readings_d['root_segment']
 
@@ -99,6 +104,8 @@ def build_Center_Line(x,y):
     distal_list = []
     Center_list = []
     distal_list2 = []
+    test_common = []
+    test_internal = []
     C_firstz = root['cross_sections'][0]['position'][2]
     C_firsty =  root['cross_sections'][0]['position'][1]
     C_firstx =  root['cross_sections'][0]['position'][0]
@@ -106,15 +113,34 @@ def build_Center_Line(x,y):
     
     Lower_end = borders[0][2]+(-1)*C_firstz
     Hihger_end = borders[1][2]+(-1)*C_firstz
-    
+    #root = readings_d['initialization_points']
+    #distal = root['distal_segments']  
 
-    
+    for section in root['cross_sections']:
+        a = section['position'][2]
+        a2 = section['position'][1]
+        a3 = section['position'][0]
+        a4 = [a3,a2,a]
+        test_common.append(a4)
+
+    for section in distal[1]['cross_sections']:
+        c = section['position'][2]
+        c2 = section['position'][1]
+        c3 = section['position'][0]
+        c4 = [c3,c2,c]
+        test_internal.append(c4)
+    """
     for section in root['cross_sections']:
         a = section['position'][2]+(-1)*C_firstz
         a2 = section['position'][1]+(-1)*C_firsty
         a3 = section['position'][0]+(-1)*C_firstx
         a4 = [a3,a2,a]
         Center_list.append(a4)
+        a = section['position'][2]
+        a2 = section['position'][1]
+        a3 = section['position'][0]
+        a4 = [a3,a2,a]
+        test.append(a4)
     
     for section in distal[0]['cross_sections']:
        
@@ -123,6 +149,11 @@ def build_Center_Line(x,y):
         b3 = section['position'][0]+(-1)*C_firstx
         b4 = [b3,b2,b]
         distal_list.append(b4)
+        #b = section['position'][2]
+        #b2 = section['position'][1]
+        #b3 = section['position'][0]
+        #b4 = [b3,b2,b]
+        #test.append(b4)
 
     for section in distal[1]['cross_sections']:
         c = section['position'][2]+(-1)*C_firstz
@@ -130,23 +161,155 @@ def build_Center_Line(x,y):
         c3 = section['position'][0]+(-1)*C_firstx
         c4 = [c3,c2,c]
         distal_list2.append(c4)
-    firstz = root['cross_sections'][0]['position'][2]
-    lastz = distal[1]['cross_sections'][-1]['position'][2]
-    hej = (-1)*firstz - (-1)*lastz
+        c = section['position'][2]
+        c2 = section['position'][1]
+        c3 = section['position'][0]
+        c4 = [c3,c2,c]
+        test.append(c4)
+    """
+    #print(Center_list)
     
-    converterz = (labelVolume.shape[2]-1)/hej
+    #print(distal_list2)
+    firstzC = root['cross_sections'][0]['position'][2]
+    lastzC = root['cross_sections'][-1]['position'][2]
+    firstyC = root['cross_sections'][0]['position'][1]
+    lastyC = root['cross_sections'][-1]['position'][1]
+    firstxC = root['cross_sections'][0]['position'][0]
+    lastxC = root['cross_sections'][-1]['position'][0]
+
+    firstzI = distal[1]['cross_sections'][0]['position'][2]
+    lastzI = distal[1]['cross_sections'][-1]['position'][2]
+    firstyI = distal[1]['cross_sections'][0]['position'][1]
+    lastyI = distal[1]['cross_sections'][-1]['position'][1]
+    firstxI = distal[1]['cross_sections'][0]['position'][0]
+    lastxI = distal[1]['cross_sections'][-1]['position'][0]
+    #lastx = distal[0]['cross_sections'][-1]['position'][0]
+    
+    #print(lastx)
+    #lastx = readings_d['initialization_points']['distal_segments'][1]['cross_sections'][-1]['position'][0]
+    #lastx = readings_d['initialization_points']['distal_segments'][0]['cross_sections'][-1]['position'][0]
+    #lastx = distal[1]['cross_sections'][-1]['position'][0]
+    #print(firstz-lastz)
+    #print(firsty-lasty)
+    #print(firstx-lastx)
+    #print(lastz)
+    #print(lasty)
+    #print(lastx)
+    for i in test_common:
+        i[0] = i[0]-firstxC
+        i[1] = i[1]-firstyC
+        i[2] = i[2]-firstzC
+
+    for i in test_internal:
+        i[0] = i[0]-firstxI
+        i[1] = i[1]-firstyI
+        i[2] = i[2]-firstzI
+    #print(test_internal)
+    hej = abs(firstzC - lastzC)
+    #print(abs(firstxC-lastzI)/hej)
+    hej2 = abs(firstyC-lastyC)
+    hej3 = abs(firstxC-lastxC)
+
+    haha = (labelVolume.shape[2]-1)/(abs(firstzC-lastzI))
+    print(haha)
+    hehe = round(abs(firstzC-lastzC)*haha)
+    print(hehe)
+    hoho = round(abs(firstzC-firstzI)*haha)
+    #print(hej,hej2,hej3)
+    #print(x,y,x2,y2)
+    print(hoho)
+    middle_center = Find_center(labelVolume[:,:,hoho])
+    print(middle_center)
+    #converterz = (labelVolume.shape[2]-1)/hej
+    converterz = (hehe/hej)
+    convertery = (abs(y-middle_center[1])/hej2)
+    converterx = (abs(x-middle_center[0])/hej3)
+    print(converterx)
+    print(convertery)
+    print(converterz)
+
+    hej4 = abs(firstzI-lastzI)
+    hej5 = abs(firstyI-lastyI)
+    hej6 = abs(firstxI-lastxI)
+
+    converterz2 = ((labelVolume.shape[2]-hehe-1)/hej4)
+    convertery2 = (abs(middle_center[1]-y2)/hej5)
+    converterx2 = (abs(middle_center[0]-x2)/hej6)
+    #print(hej4)
+    #print(hej5)
+    #print(hej6)
+    #print(converterz2)
+    #print(convertery2)
+    #print(converterx2)
+    #print(Center_list[55:70])
     Hihger_end = round(Hihger_end*converterz)
     Lower_end = round(Lower_end*converterz)
-    
-    
-    Center_list = [[round(j*converterz) for j in i] for i in Center_list]
+    test = [[i[0]*converterx2+x,i[1]*convertery+y,i[2]*converterz] for i in test_common]
+    test = [[round(i[0]),round(i[1]),round(i[2])] for i in test]
+    print(test)
+    #xt = []
+    test2 = [[i[0]*converterx2+middle_center[0],i[1]*convertery2+middle_center[1],i[2]*converterz2+hoho-2] for i in test_internal]
+    test2 = [[round(i[0]),round(i[1]),round(i[2])] for i in test2]
+    #yt = []
+    #zt = []
+    print(test2)
+    #for i in test:
+       # yt.append(i[1])
+       # xt.append(i[0])
+       # zt.append(i[2])
+    test = test + test2
+    #print(max(x),min(x))
+    #for i in Center_list:
+    #    ylist.append(i[1])
+    #    xlist.append(i[0])
+    #    zlist.append(i[2])
+    #heja = max(np.where(labelVolume == 6)[0])- min(np.where(labelVolume == 6)[0])
+    #hoja = max(np.where(labelVolume == 6)[1])- min(np.where(labelVolume == 6)[1])
+    #haja = max(np.where(labelVolume == 6)[2])- min(np.where(labelVolume == 6)[2])
+    #converterx = (max(xt)-min(xt))/(heja-10)
+    #convertery = (max(yt)-min(yt))/(hoja-10)
+    #Center_list = [[round(j/converterz) for j in i] for i in Center_list]
+    #Center_list = [[i[0]+x,i[1]+y,i[2]] for i in Center_list]
+    for i in Center_list:
+        i[0] = round(i[0]/converterx)
+        i[1] = round(i[1]/convertery)
+        i[2] = round(i[2]/converterz)
+    #print(Center_list)
     Center_list = [[i[0]+x,i[1]+y,i[2]] for i in Center_list]
-    distal_list = [[round(j*converterz) for j in i] for i in distal_list]
-    distal_list = [[i[0]+x,i[1]+y,i[2]] for i in distal_list]
-    distal_list2 = [[round(j*converterz) for j in i] for i in distal_list2]
+    #test = [[round(i[0]+x)*4,round(i[1]+y)*4,round(i[2])*4] for i in test]
+    #print
+    #print(Center_list[55:70])
+    #for i in Center_list:
+    #    ylist2.append(i[1])
+    #    xlist2.append(i[0])
+    #    zlist2.append(i[2])
+    #print(min(ylist),max(ylist))
+    #print(min(ylist2),max(ylist2))
+    #print(min(xlist),max(xlist))
+    #print(min(xlist2),max(xlist2))
+    #print(min(zlist),max(zlist))
+    #print(min(zlist2),max(zlist2))
+    #print(min(xt) , max(xt), max(xt)-min(xt))
+    #print(min(yt),max(yt), max(yt)-min(yt))
+    #print(min(zt),max(zt),max(zt)-min(zt))
+    for i in distal_list2:
+        i[0] = round(i[0]/converterx)
+        i[1] = round(i[1]/convertery)
+        i[2] = round(i[2]/converterz)
+    #print(distal_list2)
     distal_list2 = [[i[0]+x,i[1]+y,i[2]] for i in distal_list2]
+    for i in distal_list:
+        i[0] = round(i[0]/converterx)
+        i[1] = round(i[1]/convertery)
+        i[2] = round(i[2]/converterz)
+    distal_list = [[i[0]+x,i[1]+y,i[2]] for i in distal_list]
+    #distal_list = [[round(j/converterz) for j in i] for i in distal_list]
+    #distal_list = [[i[0]+x,i[1]+y,i[2]] for i in distal_list]
+    #distal_list2 = [[round(j/converterz) for j in i] for i in distal_list2]
+    #print(distal_list2)
+    #distal_list2 = [[i[0]+x,i[1]+y,i[2]] for i in distal_list2]
 
-    return Center_list ,distal_list,distal_list2,Lower_end,Hihger_end
+    return Center_list ,distal_list,distal_list2,Lower_end,Hihger_end,test
 
 
 
@@ -154,7 +317,7 @@ def build_Center_Line(x,y):
 
 
 def Find_center(image):
-    centroid = np.mean(np.argwhere(image),axis=0)
+    centroid = np.mean(np.argwhere(image==6),axis=0)
     x = round(centroid[0])
     y = round(centroid[1])
     return [x,y]
@@ -304,37 +467,74 @@ def vertical_slices(Center_line, labelVolume, H, L):
                         slice[i,j,k-2] = volume_short[i,j,h]
                 
                         
-    return slice, volume_short
+    return slice, volume_short, labelVolume
+def main():
+    f1,f2,f3,f4,f5 = create_file_paths("BiKE_0850")
+    labelVolume, reading, LumenData = create_volume(f1,f2,f3,f4,f5)
+    cent = Find_center(labelVolume[:,:,0])
+    end_center = Find_center(labelVolume[:,:,labelVolume.shape[2]-1])
+    #middle_center = Find_center(labelVolume[:,:,115])
+    #print(middle_center)
+    print(end_center)
+    print(cent)
+    C,d,d2,L,H,test = build_Center_Line(cent[0],cent[1],end_center[0],end_center[1],reading,labelVolume)
 
-
-#cent = Find_center(labelVolume[:,:,0])
-#C,d,d2,L,H = build_Center_Line(cent[0],cent[1])
-
-#Total = C + d2
-#slice, label2 = vertical_slices(Total, labelVolume,H,L)
-#label2 = LumenData[:,:,L:H]
-#print(slice.shape)
-#np.save(filepath4[0] + r"Slices.txt.npy", slice)
+    Total = C + d2 + d
+    
+    #slice, label2, labelVolume2 = vertical_slices(Total, labelVolume,H,L)
+    """
+    label2 = LumenData[:,:,L:H]
+    #print(slice.shape)
+    np.save(f4[0] + r"Slices.txt.npy", slice)
 #slice = np.load(filepath4[0] + r"Slices.txt.npy")
-#np.save(filepath4[0] + r"Plaque_volume.txt", label2)
-slice = np.load(filepath4[0] + r"Slices.txt.npy")
-Unwrap_list = np.zeros([360,30, slice.shape[2]])
+    np.save(f4[0] + r"Plaque_volume.txt", label2)
+    slice = np.load(f4[0] + r"Slices.txt.npy")
+    Unwrap_list = np.zeros([360,30, slice.shape[2]])
 
-for i in range(slice.shape[2]):
-    print(i)
-    first_half,second_half = unwrap_slice(slice[:,:,i])
-    total_unwrap = scale_unwrap(first_half, second_half)
-    Unwrap_list[:,:,i] = total_unwrap
+    for i in range(slice.shape[2]):
+        print(i)
+        first_half,second_half = unwrap_slice(slice[:,:,i])
+        total_unwrap = scale_unwrap(first_half, second_half)
+        Unwrap_list[:,:,i] = total_unwrap
 
-np.save(filepath4[0] +r"Unwraps.txt.npy", Unwrap_list)
-#unwraps = np.load(filepath4[0] +r"Unwraps.txt.npy")
-#slice = np.load(filepath4[0] + r"Slices.txt.npy")
+    np.save(f4[0] +r"Unwraps.txt.npy", Unwrap_list)
+    unwraps = np.load(f4[0] +r"Unwraps.txt.npy")
+    slice = np.load(f4[0] + r"Slices.txt.npy")
 #print(slice.shape)
-with napari.gui_qt():
-    viewer = napari.Viewer()
+
+    """
+    """
+    from mpl_toolkits.mplot3d import Axes3D
+    fig = plt.figure()
+    ax = fig.add_subplot(projection = '3d')
+
+# plotting the points
+    pts = test
+    for p in pts:
+        ax.scatter(p[0], p[1], p[2], zdir='z', c='r')
+
+# plotting lines for each point pair
+
+
+    ax.legend()
+    ax.set_xlim3d(0, 357)
+    ax.set_ylim3d(0, 357)
+    ax.set_zlim3d(0, 357)
+
+    plt.show()
+    """
+    for i in test:
+    #   print(i)
+       labelVolume[i[0],i[1],i[2]] = 7
+    with napari.gui_qt():
+        viewer = napari.Viewer()
     #viewer.add_image(total_unwrap)
-    viewer.add_image(slice)
-    #viewer.add_image(labelVolume)
+        #viewer.add_image(slice)
+        viewer.add_image(labelVolume)
     #viewer.add_image(unwraps)
-    viewer.add_image(Unwrap_list)
-napari.run()
+        #viewer.add_image(unwraps)
+    napari.run()
+
+    
+    
+main()
