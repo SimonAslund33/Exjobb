90b1e074bc9ad4ea3fc75b6fd479ed243dadf499
SimonAslund33 <114583924+SimonAslund33@users.noreply.github.com>
2023-10-02 12:45:44 +0200

    Add files via upload

--- feature_extractions.py
+++ feature_extractions.py
@@ -4,6 +4,7 @@ import pandas as pd
 import xlwings as xw
 import matplotlib.pyplot as plt
 import seaborn as sns
+import cv2
 
 slice = np.load(r'C:\Users\simon\OneDrive\Desktop\ExJobbPlaqueInfo\BiKE_0846\wi-266BCEEA\RightCarotid\Slices.txt.npy')
 unwraps = np.load(r"C:\Users\simon\OneDrive\Desktop\ExJobbPlaqueInfo\BiKE_0846\wi-266BCEEA\RightCarotid\Unwraps.txt.npy")
@@ -13,6 +14,28 @@ plaque_volume = np.load(r"C:\Users\simon\OneDrive\Desktop\ExJobbPlaqueInfo\BiKE_
 xls = pd.ExcelFile(r'C:\Users\simon\Downloads\BiKE_imported_csv.xlsx')
 df1 = pd.read_excel(xls, 'BiKE Elucid plus operation')
 
+def new_center(image):
+    centers = []
+    for j in range(image.shape[2]):
+        thresh = cv2.inRange(image[:,:,j], 6, 6)
+
+        contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+        CL = np.where(image[:,:,j] == 7)
+        print(contours)
+        distance = 1000
+        for i in contours:
+            point = (sum(i)/len(i))
+            print(point)
+            point = [round(j) for j in point[0]]
+            dist = np.sqrt( (point[1] - CL[0])**2 + (point[0] - CL[1])**2 )
+            print(dist)
+            if dist < distance:
+                distance = dist
+                closest_point = point
+            
+        centers.append(closest_point)
+        
+    return centers
 
 PatientID = []
 CalcVols = []
@@ -61,8 +84,12 @@ def calcvol():
     ax.set_xlabel( "CalcVol")
     ax.set_ylabel( "count")
     plt.show()
-
-
+cent = new_center(slice)
+    
+#print(cent)  
+for i in range(slice.shape[2]):
+    #print(cent[i])
+    slice[cent[i][1],cent[i][0],i] = 8
     
 with napari.gui_qt():
     viewer = napari.Viewer()
@@ -70,5 +97,5 @@ with napari.gui_qt():
     viewer.add_image(slice)
     #viewer.add_image(labelVolume)
     #viewer.add_image(unwraps)
-    #viewer.add_image(Unwrap_list)
+    #viewer.add_image(cent)
 napari.run()
